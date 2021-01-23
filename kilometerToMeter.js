f7cb2ec4371b8653207d8c05a6c49d2a6832c7d2
Nur Hasan <nurhasanfoysal@gmail.com>
2021-01-23 21:59:04 +0600

    first commit

---
+++ kilometerToMeter.js
@@ -0,0 +1,13 @@
+
+
+// https://github.com/nurhasan4421/third-assignment
+
+function kilometerToMeter(kilometer){
+    var Meter = kilometer  * 1000;
+    return Meter;
+}
+var firstMeter = kilometerToMeter(5);
+console.log(firstMeter)
+
+var secondMeter = kilometerToMeter(10);
+console.log(secondMeter)
